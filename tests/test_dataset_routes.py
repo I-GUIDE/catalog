47645def1a634639867a016195ae9ed174d3e25e
pkdash <pkdash_reena@hotmail.com>
2023-05-14 19:47:37 -0400

    tests for api routes

---
+++ tests/test_dataset_routes.py
@@ -0,0 +1,43 @@
+import pytest
+
+from api.models.catalog import DatasetMetadataDOC
+from tests import client_test, dataset_data, core_data, change_test_dir
+
+pytestmark = pytest.mark.asyncio
+
+# TODO: add tests for the routes put and delete
+
+
+async def test_dataset(client_test, dataset_data):
+    """Testing the dataset routes for post and get"""
+
+    # add a dataset record to the db
+    response = await client_test.post("api/catalog/dataset", json=dataset_data)
+    assert response.status_code == 201
+    response_data = response.json()
+    record_id = response_data.pop('_id')
+
+    # prepare the expected dataset data
+    dataset_data['funding'] = None
+    dataset_data['publisher'] = None
+    dataset_data['distribution']['comment'] = None
+    dataset_data['creator'][0]['identifier']['name'] = None
+    dataset_data['provider']['identifier'] = None
+    dataset_data['provider']['address'] = None
+    dataset_data['provider']['parentOrganization']['identifier'] = None
+    dataset_data['spatialCoverage']['geo'] = None
+
+    # assert that the response contains the expected data
+    assert response_data == dataset_data
+
+    # retrieve the record from the db
+    response = await client_test.get(f"api/catalog/dataset/{record_id}")
+    assert response.status_code == 200
+
+
+async def test_cleanup_test_db(client_test):
+    """Cleanup the test db"""
+    try:
+        await DatasetMetadataDOC.delete_all()
+    except Exception as e:
+        pytest.fail(f"Failed to cleanup test db: {str(e)}")
