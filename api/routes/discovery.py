6cdba67ae971b5e808a4e74556c4f9229616f8e6
pkdash <pkdash_reena@hotmail.com>
2023-05-17 14:19:38 -0400

    adding additional fields to search on

--- api/routes/discovery.py
+++ api/routes/discovery.py
@@ -10,21 +10,13 @@ router = APIRouter()
 
 
 @router.get("/search")
-async def search(
-    request: Request,
-    term: str,
-    sortBy: str = None,
-    contentType: str = None,
-    providerName: str = None,
-    creatorName: str = None,
-    dataCoverageStart: int = None,
-    dataCoverageEnd: int = None,
-    publishedStart: int = None,
-    publishedEnd: int = None,
-    clusters: Union[list[str], None] = Query(default=None),
-    pageNumber: int = 1,
-    pageSize: int = 30,
-):
+async def search(request: Request, term: str, sortBy: str = None, contentType: str = None, providerName: str = None,
+                 creatorName: str = None, dataCoverageStart: int = None, dataCoverageEnd: int = None,
+                 publishedStart: int = None, publishedEnd: int = None, variableMeasured: str = None,
+                 includedInDataCatalogName: str = None, hasPartName: str = None, isPartOfName: str = None,
+                 associatedMediaName: str = None, fundingGrantName: str = None, fundingFunderName: str = None,
+                 clusters: Union[list[str], None] = Query(default=None),
+                 pageNumber: int = 1, pageSize: int = 30):
     searchPaths = ['name', 'description', 'keywords']
     highlightPaths = ['name', 'description', 'keywords', 'creator.@list.name']
     autoCompletePaths = ['name', 'description', 'keywords']
@@ -35,67 +27,173 @@ async def search(
     filters = []
 
     if publishedStart:
-        filters.append(
-            {
-                'range': {
-                    'path': 'datePublished',
-                    'gte': datetime(publishedStart, 1, 1),
-                },
-            }
-        )
+        filters.append({
+            'range': {
+                'path': 'datePublished',
+                'gte': datetime(publishedStart, 1, 1),
+            },
+        })
 
     if publishedEnd:
-        filters.append(
-            {
-                'range': {
-                    'path': 'datePublished',
-                    'lt': datetime(publishedEnd + 1, 1, 1),  # +1 to include all of the publishedEnd year
-                },
-            }
-        )
+        filters.append({
+            'range': {
+                'path': 'datePublished',
+                'lt': datetime(publishedEnd + 1, 1, 1)  # +1 to include all of the publishedEnd year
+            },
+        })
 
     if dataCoverageStart:
-        filters.append({'range': {'path': 'temporalCoverage.start', 'gte': datetime(dataCoverageStart, 1, 1)}})
+        filters.append({
+            'range': {
+                'path': 'temporalCoverage.start',
+                'gte': datetime(dataCoverageStart, 1, 1)
+            }
+        })
 
     if dataCoverageEnd:
-        filters.append({'range': {'path': 'temporalCoverage.end', 'lt': datetime(dataCoverageEnd + 1, 1, 1)}})
+        filters.append({
+            'range': {
+                'path': 'temporalCoverage.end',
+                'lt': datetime(dataCoverageEnd + 1, 1, 1)
+            }
+        })
 
     if creatorName:
-        must.append({'text': {'path': 'creator.@list.name', 'query': creatorName}})
+        must.append({
+            'text': {
+                'path': 'creator.@list.name',
+                'query': creatorName
+            }
+        })
 
     if providerName:
-        must.append({'text': {'path': 'provider.name', 'query': providerName}})
+        must.append({
+            'text': {
+                'path': 'provider.name',
+                'query': providerName
+            }
+        })
 
     if contentType:
-        must.append({'text': {'path': '@type', 'query': contentType}})
+        must.append({
+            'text': {
+                'path': '@type',
+                'query': contentType
+            }
+        })
+
+    if variableMeasured:
+        must.append({
+            'text': {
+                'path': 'variableMeasured',
+                'query': variableMeasured
+            }
+        })
+
+    if includedInDataCatalogName:
+        must.append({
+            'text': {
+                'path': 'includedInDataCatalog.@list.name',
+                'query': includedInDataCatalogName
+            }
+        })
+
+    if hasPartName:
+        must.append({
+            'text': {
+                'path': 'hasPart.@list.name',
+                'query': hasPartName
+            }
+        })
+
+    if isPartOfName:
+        must.append({
+            'text': {
+                'path': 'isPartOf.@list.name',
+                'query': isPartOfName
+            }
+        })
+
+    if associatedMediaName:
+        must.append({
+            'text': {
+                'path': 'associatedMedia.@list.name',
+                'query': associatedMediaName
+            }
+        })
+
+    if fundingGrantName:
+        must.append({
+            'text': {
+                'path': 'funding.@list.grant.name',
+                'query': fundingGrantName
+            }
+        })
+
+    if fundingFunderName:
+        must.append({
+            'text': {
+                'path': 'funding.@list.funder.name',
+                'query': fundingFunderName
+            }
+        })
 
     stages.append(
         {
             '$search': {
                 'index': 'fuzzy_search',
-                'compound': {'filter': filters, 'should': should, 'must': must},
-                'highlight': {'path': highlightPaths},
+                'compound': {
+                    'filter': filters,
+                    'should': should,
+                    'must': must
+                },
+                'highlight': {'path': highlightPaths}
             }
         }
     )
 
     if clusters:
-        stages.append({'$match': {'clusters': {'$all': clusters}}})
+        stages.append({
+            '$match': {
+                'clusters': {
+                    '$all': clusters
+                }
+            }
+        })
 
     # Sort needs to happen before pagination
     if sortBy:
-        stages.append({'$sort': {sortBy: 1}})
+        stages.append({
+            '$sort': {
+                sortBy: 1
+            }
+        })
 
-    stages.append({'$skip': (pageNumber - 1) * pageSize})
     stages.append(
-        {'$limit': pageSize},
+        {
+            '$skip': (pageNumber - 1) * pageSize
+        }
     )
-    stages.append({'$unset': ['_id']})
     stages.append(
-        {'$set': {'score': {'$meta': 'searchScore'}, 'highlights': {'$meta': 'searchHighlights'}}},
+        {
+            '$limit': pageSize
+        },
+    )
+    stages.append(
+        {
+            '$unset': ['_id']
+        }
+    )
+    stages.append(
+        {
+            '$set': {
+                'score': {'$meta': 'searchScore'},
+                'highlights': {'$meta': 'searchHighlights'}
+            }
+        },
     )
 
-    result = await request.app.mongodb["catalog"].aggregate(stages).to_list(pageSize)
+    result = await request.app.mongodb["cznet"].aggregate(stages).to_list(pageSize)
     return result
 
 
@@ -110,33 +208,39 @@ async def typeahead(request: Request, term: str, pageSize: int = 30):
             '$search': {
                 'index': 'fuzzy_search',
                 'compound': {
-                    'should': [
-                        {'autocomplete': {'query': term, 'path': 'description', 'fuzzy': {'maxEdits': 1}}},
-                        {'autocomplete': {'query': term, 'path': 'name', 'fuzzy': {'maxEdits': 1}}},
-                        {'autocomplete': {'query': term, 'path': 'keywords', 'fuzzy': {'maxEdits': 1}}},
-                    ]
+                    'should': should
                 },
-                'highlight': {'path': ['description', 'name', 'keywords']},
+                'highlight': {
+                    'path': ['description', 'name', 'keywords']
+                }
             }
-        },
-        {
+        }, {
             '$project': {
                 'name': 1,
                 'description': 1,
                 'keywords': 1,
-                'highlights': {'$meta': 'searchHighlights'},
-                '_id': 0,
+                'highlights': {
+                    '$meta': 'searchHighlights'
+                },
+                '_id': 0
             }
-        },
+        }
     ]
-    result = await request.app.mongodb["typeahead"].aggregate(stages).to_list(pageSize)
+    result = await request.app.mongodb["cznet"].aggregate(stages).to_list(pageSize)
     return result
 
 
 @router.get("/csv")
 async def sanitize(request: Request):
-    project = [{'$project': {'name': 1, 'description': 1, 'keywords': 1, '_id': 0}}]
-    json_response = await request.app.mongodb["catalog"].aggregate(project).to_list(None)
+    project = [{
+        '$project': {
+            'name': 1,
+            'description': 1,
+            'keywords': 1,
+            '_id': 0
+        }
+    }]
+    json_response = await request.app.mongodb["cznet"].aggregate(project).to_list(None)
     df = pandas.read_json(json.dumps(json_response))
     filename = "file.csv"
     df.to_csv(filename)
