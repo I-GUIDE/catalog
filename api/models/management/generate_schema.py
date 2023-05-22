dd6e9588b22d2dd2aad2c743adefbc5fce895512
Scott Black <sblack@cuahsi.org>
2023-05-21 21:49:05 -0600

    update default schema update path

--- api/models/management/generate_schema.py
+++ api/models/management/generate_schema.py
@@ -1,47 +1,29 @@
 import json
 import os
-from typing import Any, Dict
 
 import typer
-from pydantic import BaseModel
 
 from api.models.schema import DatasetSchema
 
 
-def main(output_name: str = "api/models/schema.json"):
+def main(output_name: str = "api/dataset_schema.json"):
     schema = DatasetSchema.schema()
-    schema = embed_definitions(schema)
+    json_schema = DatasetSchema.schema_json(indent=2)
+    # Have to run it a few times for the definitions to get updated before inserted into another model
+    while "#/definitions/" in json_schema:
+        for definition in schema["definitions"]:
+            class_definition = schema["definitions"][definition]
+            json_schema = json_schema.replace(f'"#/definitions/{definition}"', json.dumps(class_definition, indent=2))
+    embedded_schema = json.loads(json_schema)
     current_directory = absolute_directory(output_name)
     with open(current_directory, "w") as f:
-        f.write(json.dumps(schema, indent=2))
-
-
-def embed_definitions(schema: Dict[str, Any]):
-    for _, property in schema["properties"].items():
-        any_of = property['anyOf'] if 'anyOf' in property else None
-        if any_of:
-            for item in any_of:
-                # print(item)
-                ref: str = item['$ref'] if '$ref' in item else None
-                if ref:
-                    # print(ref)
-                    definition = ref[len("#/definitions/") :]
-                    print(definition)
-
-    # for definition in schema["definitions"]:
-    #    print(definition)
-
-    # for key, val in schema.items():
-    # val = schema['properties'][key]
-    # print(key)
-    # print(val)
-    return schema
+        f.write(json.dumps(embedded_schema, indent=2))
 
 
 def absolute_directory(output_name):
     current_directory = os.getcwd()
     current_directory = os.path.join(current_directory, output_name)
-    return current_directory
+    return "/" + current_directory
 
 
 if __name__ == "__main__":
