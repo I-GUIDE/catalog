fdfeeb4ce0f730ac12a939b905f3d5d4fa77c62c
pkdash <pkdash_reena@hotmail.com>
2023-05-22 11:59:27 -0400

    removing search on clusters

--- api/main.py
+++ api/main.py
@@ -50,4 +50,4 @@ openapi_schema = get_openapi(
 app.openapi_schema = openapi_schema
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5002)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
