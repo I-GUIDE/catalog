2dfad3d4a52c7ad8eb286568e15d6d9f7e6e7c95
Scott Black <sblack@cuahsi.org>
2023-05-23 18:47:21 -0600

    add TODOs for title/description in the schema

--- api/models/schema.py
+++ api/models/schema.py
@@ -26,59 +26,61 @@ class SchemaBaseModel(BaseModel):
 
 
 class CreativeWork(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="CreativeWork")
-    name: str
+    type: str = Field(alias="@type", const=True, default="CreativeWork", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
 
 
 class PropertyValue(SchemaBaseModel):
-    id: HttpUrl = Field(alias="@id")
-    type: str = Field(alias="@type", const=True, default="PropertyValue")
-    name: Optional[str]
-    propertyID: Optional[HttpUrl]
-    value: str
-    url: HttpUrl
+    id: HttpUrl = Field(alias="@id", title="TODO", description="TODO")
+    type: str = Field(alias="@type", const=True, default="PropertyValue", title="TODO", description="TODO")
+    name: Optional[str] = Field(title="TODO", description="TODO")
+    propertyID: Optional[HttpUrl] = Field(title="TODO", description="TODO")
+    value: str = Field(title="TODO", description="TODO")
+    url: HttpUrl = Field(title="TODO", description="TODO")
 
 
 Identifier = Union[str, HttpUrl, PropertyValue]
 
 
 class Person(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="Person")
-    name: str
-    email: Optional[EmailStr]
-    identifier: Optional[List[Union[Identifier, HttpUrl]]]
+    type: str = Field(alias="@type", const=True, default="Person", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
+    email: Optional[EmailStr] = Field(title="TODO", description="TODO")
+    identifier: Optional[List[Identifier]] = Field(title="TODO", description="TODO")
 
 
 class Organization(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="Organization")
-    name: str
-    url: Optional[HttpUrl]
-    identifier: Optional[List[Union[Identifier, HttpUrl]]]
-    address: Optional[str]  # Should address be a string or another constrained type?
+    type: str = Field(alias="@type", const=True, default="Organization", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
+    url: Optional[HttpUrl] = Field(title="TODO", description="TODO")
+    identifier: Optional[List[Identifier]] = Field(title="TODO", description="TODO")
+    address: Optional[str] = Field(
+        title="TODO", description="TODO"
+    )  # Should address be a string or another constrained type?
 
 
 class ProviderID(SchemaBaseModel):
-    id: HttpUrl = Field(alias="@id")
+    id: HttpUrl = Field(alias="@id", title="TODO", description="TODO")
 
 
 class ProviderOrganization(Organization):
-    parentOrganization: Optional[Organization]
+    parentOrganization: Optional[Organization] = Field(title="TODO", description="TODO")
 
 
 class DefinedTerm(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="DefinedTerm")
-    name: str
-    description: str
+    type: str = Field(alias="@type", const=True, default="DefinedTerm", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
+    description: str = Field(title="TODO", description="TODO")
 
 
 class KeywordTerm(DefinedTerm):
-    inDefinedTermSet: HttpUrl
+    inDefinedTermSet: HttpUrl = Field(title="TODO", description="TODO")
 
 
 class HasPart(CreativeWork):
-    description: str
-    identifier: Identifier
-    creator: Optional[Union[Person, Organization]]
+    description: str = Field(title="TODO", description="TODO")
+    identifier: Identifier = Field(title="TODO", description="TODO")
+    creator: Optional[Union[Person, Organization]] = Field(title="TODO", description="TODO")
 
 
 class IsPartOf(HasPart):
@@ -86,12 +88,12 @@ class IsPartOf(HasPart):
 
 
 class SubjectOf(CreativeWork):
-    url: HttpUrl
-    encodingFormat: str
+    url: HttpUrl = Field(title="TODO", description="TODO")
+    encodingFormat: str = Field(title="TODO", description="TODO")
 
 
 class License(CreativeWork):
-    url: HttpUrl
+    url: HttpUrl = Field(title="TODO", description="TODO")
 
 
 class LanguageEnum(str, Enum):
@@ -100,10 +102,10 @@ class LanguageEnum(str, Enum):
 
 
 class Grant(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="MonetaryGrant")
-    name: str
-    url: HttpUrl
-    funder: Union[Person, Organization]
+    type: str = Field(alias="@type", const=True, default="MonetaryGrant", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
+    url: HttpUrl = Field(title="TODO", description="TODO")
+    funder: Union[Person, Organization] = Field(title="TODO", description="TODO")
 
 
 class TimeInterval(str):
@@ -155,9 +157,9 @@ class TimeInterval(str):
 
 
 class GeoCoordinates(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="GeoCoordinates")
-    latitude: float
-    longitude: float
+    type: str = Field(alias="@type", const=True, default="GeoCoordinates", title="TODO", description="TODO")
+    latitude: float = Field(title="TODO", description="TODO")
+    longitude: float = Field(title="TODO", description="TODO")
 
     @validator('latitude')
     def validate_latitude(cls, v):
@@ -173,11 +175,11 @@ class GeoCoordinates(SchemaBaseModel):
 
 
 class GeoShape(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="GeoShape")
+    type: str = Field(alias="@type", const=True, default="GeoShape", title="TODO", description="TODO")
 
 
 class Line(GeoShape):
-    line: str
+    line: str = Field(title="TODO", description="TODO")
 
     @validator('line')
     def validate_line(cls, v):
@@ -201,7 +203,7 @@ class Line(GeoShape):
 
 
 class Polygon(GeoShape):
-    polygon: str
+    polygon: str = Field(title="TODO", description="TODO")
 
     @validator('polygon')
     def validate_polygon(cls, v):
@@ -225,18 +227,18 @@ class Polygon(GeoShape):
 
 
 class Place(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="Place")
-    name: Optional[str]
-    address: Optional[str]
-    geo: Optional[Union[Line, Polygon, GeoCoordinates]]
+    type: str = Field(alias="@type", const=True, default="Place", title="TODO", description="TODO")
+    name: Optional[str] = Field(title="TODO", description="TODO")
+    address: Optional[str] = Field(title="TODO", description="TODO")
+    geo: Optional[Union[Line, Polygon, GeoCoordinates]] = Field(title="TODO", description="TODO")
 
 
 class MediaObject(SchemaBaseModel):
-    type: str = Field(alias="@type", const=False, default="MediaObject")
-    contentUrl: HttpUrl
-    encodingFormat: str  # TODO enum for encoding formats
-    contentSize: str
-    name: str
+    type: str = Field(alias="@type", const=False, default="MediaObject", title="TODO", description="TODO")
+    contentUrl: HttpUrl = Field(title="TODO", description="TODO")
+    encodingFormat: str = Field(title="TODO", description="TODO")  # TODO enum for encoding formats
+    contentSize: str = Field(title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
 
     @validator('contentSize')
     def validate_content_size(cls, v):
@@ -267,93 +269,111 @@ class MediaObject(SchemaBaseModel):
 
 
 class CoreMetadata(SchemaBaseModel):
-    context: HttpUrl = Field(alias='@context', default='https://schema.org')
-    type: str = Field(alias="@type", const=True, default="Dataset")
-    name: str = Field(description="The name or title of the record.")
-    description: str = Field(description="The description or abstract of the record.")
-    url: HttpUrl = Field(description="The url of the record.")
-    identifier: List[Union[str, HttpUrl, PropertyValue]] = Field(description="Any kind of identifier for the record.")
-    creator: List[Union[Person, Organization]] = Field(description="Person or organization that created the work.")
-    dateCreated: Union[date, datetime] = Field(description="The date on which the work was created.")
+    context: HttpUrl = Field(alias='@context', default='https://schema.org', title="TODO", description="TODO")
+    type: str = Field(alias="@type", const=True, default="Dataset", title="TODO", description="TODO")
+    name: str = Field(description="The name or title of the record.", title="TODO")
+    description: str = Field(description="The description or abstract of the record.", title="TODO")
+    url: HttpUrl = Field(description="The url of the record.", title="TODO")
+    identifier: List[Union[str, HttpUrl, PropertyValue]] = Field(
+        description="Any kind of identifier for the record.", title="TODO"
+    )
+    creator: List[Union[Person, Organization]] = Field(
+        description="Person or organization that created the work.", title="TODO"
+    )
+    dateCreated: Union[date, datetime] = Field(description="The date on which the work was created.", title="TODO")
     keywords: List[Union[KeywordTerm, str, HttpUrl]] = Field(
-        min_items=1, description="Keywords or tags used to describe the dataset, delimited by commas."
+        min_items=1, description="Keywords or tags used to describe the dataset, delimited by commas.", title="TODO"
     )
     license: Union[License, HttpUrl] = Field(
-        description="A license document that applies to the content, typically indicated by a URL."
+        description="A license document that applies to the content, typically indicated by a URL.", title="TODO"
     )
     provider: Union[ProviderOrganization, Person, ProviderID] = Field(
-        description="The service provider, service operator, or service performer."
+        description="The service provider, service operator, or service performer.", title="TODO"
     )
     publisher: Optional[Union[ProviderOrganization, Person, ProviderID]] = Field(
-        description="The publisher of the record."
+        description="The publisher of the record.", title="TODO"
     )
-    datePublished: Optional[Union[date, datetime]] = Field(description="Date of first publication for the record.")
+    datePublished: Optional[Union[date, datetime]] = Field(
+        description="Date of first publication for the record.", title="TODO"
+    )
     subjectOf: Optional[List[SubjectOf]] = Field(
-        description="A CreativeWork about the record - e.g., a related metadata document describing the record."
+        description="A CreativeWork about the record - e.g., a related metadata document describing the record.",
+        title="TODO",
     )
     version: Optional[Union[float, str]] = Field(
-        description="The version of the record."
+        description="The version of the record.", title="TODO"
     )  # TODO find something better than float for number
-    inLanguage: Optional[Union[LanguageEnum, str]] = Field(description="The language of the content of the record.")
+    inLanguage: Optional[Union[LanguageEnum, str]] = Field(
+        description="The language of the content of the record.", title="TODO"
+    )
     creativeWorkStatus: Optional[Union[DefinedTerm, str]] = Field(
-        description="The status of a creative work in terms of its stage in a lifecycle. Example terms include Incomplete, Draft, Published, Obsolete. Some organizations define a set of terms for the stages of their publication lifecycle."
+        description="The status of a creative work in terms of its stage in a lifecycle. Example terms include Incomplete, Draft, Published, Obsolete. Some organizations define a set of terms for the stages of their publication lifecycle.",
+        title="TODO",
     )
     dateModified: Optional[Union[date, datetime]] = Field(
-        description="The date on which the CreativeWork was most recently modified or updated."
+        description="The date on which the CreativeWork was most recently modified or updated.", title="TODO"
     )
     funding: Optional[List[Grant]] = Field(
-        description="A Grant that directly or indirectly provide funding or sponsorship for creation of the dataset."
+        description="A Grant that directly or indirectly provide funding or sponsorship for creation of the dataset.",
+        title="TODO",
     )
     temporalCoverage: Optional[TimeInterval] = Field(
-        description="The temporalCoverage of a CreativeWork indicates the period that the content applies to, i.e. that it describes, either as a DateTime or as a textual string indicating a time period in ISO 8601 time interval format."
+        description="The temporalCoverage of a CreativeWork indicates the period that the content applies to, i.e. that it describes, either as a DateTime or as a textual string indicating a time period in ISO 8601 time interval format.",
+        title="TODO",
     )
     spatialCoverage: Optional[Place] = Field(
-        description="The spatialCoverage of a CreativeWork indicates the place(s) which are the focus of the content. It is a subproperty of contentLocation intended primarily for more technical and detailed materials. For example with a Dataset, it indicates areas that the dataset describes: a dataset of New York weather would have spatialCoverage which was the place: the state of New York."
+        description="The spatialCoverage of a CreativeWork indicates the place(s) which are the focus of the content. It is a subproperty of contentLocation intended primarily for more technical and detailed materials. For example with a Dataset, it indicates areas that the dataset describes: a dataset of New York weather would have spatialCoverage which was the place: the state of New York.",
+        title="TODO",
     )
     hasPart: Optional[List[HasPart]] = Field(
-        description="Indicates an record or CreativeWork that is part of this record."
+        description="Indicates an record or CreativeWork that is part of this record.", title="TODO"
     )
     isPartOf: Optional[List[IsPartOf]] = Field(
-        description="Indicates an record or CreativeWork that this record, or CreativeWork (in some sense), is part of."
+        description="Indicates an record or CreativeWork that this record, or CreativeWork (in some sense), is part of.",
+        title="TODO",
     )
     associatedMedia: Optional[List[MediaObject]] = Field(
-        description="A media object that encodes this CreativeWork. This property is a synonym for encoding."
+        description="A media object that encodes this CreativeWork. This property is a synonym for encoding.",
+        title="TODO",
     )
 
 
 class Distribution(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="DataDownload")
-    name: str
-    contentUrl: Optional[HttpUrl]
-    encodingFormat: Optional[list[str]]
-    contentSize: Optional[str]
-    comment: Optional[str]
+    type: str = Field(alias="@type", const=True, default="DataDownload", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
+    contentUrl: Optional[HttpUrl] = Field(title="TODO", description="TODO")
+    encodingFormat: Optional[list[str]] = Field(title="TODO", description="TODO")
+    contentSize: Optional[str] = Field(title="TODO", description="TODO")
+    comment: Optional[str] = Field(title="TODO", description="TODO")
 
 
 class VariableMeasured(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="PropertyValue")
-    name: str
-    unitText: str
+    type: str = Field(alias="@type", const=True, default="PropertyValue", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
+    unitText: str = Field(title="TODO", description="TODO")
 
 
 class IncludedInDataCatalog(SchemaBaseModel):
-    type: str = Field(alias="@type", const=True, default="DataCatalog")
-    name: str
-    description: str
-    url: HttpUrl
-    identifier: Identifier
-    creator: Union[Person, Organization]
+    type: str = Field(alias="@type", const=True, default="DataCatalog", title="TODO", description="TODO")
+    name: str = Field(title="TODO", description="TODO")
+    description: str = Field(title="TODO", description="TODO")
+    url: HttpUrl = Field(title="TODO", description="TODO")
+    identifier: Identifier = Field(title="TODO", description="TODO")
+    creator: Union[Person, Organization] = Field(title="TODO", description="TODO")
 
 
 class Dataset(SchemaBaseModel):
     distribution: List[Distribution] = Field(
-        description="A data distribution in the form of a dataset (see https://schema.org/Dataset for more information)."
+        description="A data distribution in the form of a dataset (see https://schema.org/Dataset for more information).",
+        title="TODO",
     )
     variableMeasured: Optional[List[Union[str, VariableMeasured]]] = Field(
-        description="The variableMeasured property can indicate (repeated as necessary) the variables that are measured in some dataset, either described as text or as pairs of identifier and description using PropertyValue."
+        description="The variableMeasured property can indicate (repeated as necessary) the variables that are measured in some dataset, either described as text or as pairs of identifier and description using PropertyValue.",
+        title="TODO",
     )
     includedInDataCatalog: List[IncludedInDataCatalog] = Field(
-        description="A data catalog which contains this dataset (this property was previously 'catalog', preferred name is now 'includedInDataCatalog')."
+        description="A data catalog which contains this dataset (this property was previously 'catalog', preferred name is now 'includedInDataCatalog').",
+        title="TODO",
     )
 
 
