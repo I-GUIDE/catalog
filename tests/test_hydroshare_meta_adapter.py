c354a8075be7c09d8ee3989f62588f9fd4cde1f1
pkdash <pkdash_reena@hotmail.com>
2023-07-25 16:48:33 -0400

    updating tests for hydroshare metadata adapter

--- tests/test_hydroshare_meta_adapter.py
+++ tests/test_hydroshare_meta_adapter.py
@@ -8,7 +8,7 @@ from api.models.catalog import DatasetMetadataDOC
 
 @pytest.mark.parametrize('coverage_type', ["box", "point"])
 @pytest.mark.asyncio
-async def test_hydroshare_meta_adapter_resource(hydroshare_resource_metadata, coverage_type, dataset_model):
+async def test_hydroshare_resource_meta_adapter(hydroshare_resource_metadata, coverage_type, dataset_model):
     """Test the HydroshareMetaAdapter for Composite Resource"""
 
     adapter = RepositoryMetadataAdapter.get_adapter(repository=RepositoryType.HYDROSHARE)
@@ -23,6 +23,7 @@ async def test_hydroshare_meta_adapter_resource(hydroshare_resource_metadata, co
         dataset_model(**dataset.dict())
     except ValidationError as err:
         pytest.fail(f"Catalog dataset schema model validation failed: {str(err)}")
+
     assert isinstance(dataset, DatasetMetadataDOC)
     assert dataset.name == "Testing IGUIDE Metadata Adapter for Hydroshare Resource"
     assert dataset.description == "This is a test resource - abstract"
@@ -34,15 +35,15 @@ async def test_hydroshare_meta_adapter_resource(hydroshare_resource_metadata, co
             assert cr.name in ["Smith, John", "Tseganeh Z. Gichamo"]
             if cr.name == "Smith, John":
                 assert cr.email == "john.smith@usu.edu"
-                assert cr.identifier == ["https://orcid.org/user/123"]
+                assert cr.identifier == "https://orcid.org/user/123"
             else:
-                assert cr.identifier == []
+                assert cr.identifier is None
         else:
             assert cr.type == "Organization"
             assert cr.name == "Utah State University"
             assert cr.address == "101 Logan 1st Ave"
             assert cr.url == "https://www.usu.edu"
-            assert cr.identifier == []
+
     assert dataset.dateCreated == datetime.fromisoformat("2023-05-31T03:12:34.504216+00:00")
     assert dataset.dateModified == datetime.fromisoformat("2023-06-23T21:43:10.582708+00:00")
     assert dataset.keywords == ["Logan River", "Snow water equivalent", "UEB"]
@@ -56,19 +57,22 @@ async def test_hydroshare_meta_adapter_resource(hydroshare_resource_metadata, co
     assert dataset.funding[0].identifier == "100-678-NSF"
     assert dataset.funding[0].funder.name == "NSF"
     assert dataset.funding[0].funder.url == "https://www.nsf.gov"
-    assert dataset.temporalCoverage == "2008-10-01T00:00:00/2009-06-30T21:00:00"
+    assert dataset.temporalCoverage.startDate == datetime.fromisoformat("2008-10-01T00:00:00")
+    assert dataset.temporalCoverage.endDate == datetime.fromisoformat("2009-06-30T21:00:00")
     if coverage_type == "point":
         assert dataset.spatialCoverage.name == "Logan River"
         assert dataset.spatialCoverage.geo.longitude == -111.83
         assert dataset.spatialCoverage.geo.latitude == 41.74
     else:
         assert dataset.spatialCoverage.name == "Colorado River Basin"
-        polygon = "42.11917118550477 -111.42822713925766 41.70323788190726 -111.79877261430926"
-        assert dataset.spatialCoverage.geo.polygon == polygon
+        bounding_box = "42.11917118550477 -111.42822713925766 41.70323788190726 -111.79877261430926"
+        assert dataset.spatialCoverage.geo.box == bounding_box
+
     assert len(dataset.isPartOf) == 1
     assert dataset.isPartOf[0].description == "Dash, P. (2023). DSP Collection Resource Registration Test, HydroShare"
     assert dataset.isPartOf[0].url == "http://www.hydroshare.org/resource/35cd83561bd7424c9e28f95a79af7f02"
     assert dataset.hasPart == []
+    assert dataset.citation == ["Dash, P., T. Z. Gichamo, Utah State University (2023). Testing IGUIDE Metadata Adapter for Hydroshare Resource, http://www.hydroshare.org/resource/1ee81318135c40f587d9a3e5d689daf5"]
 
     # test for resource files (MediaObject)
     assert len(dataset.associatedMedia) == 3
@@ -90,13 +94,10 @@ async def test_hydroshare_meta_adapter_resource(hydroshare_resource_metadata, co
             assert media.contentUrl == f"{media_base_url}/README.md"
             assert media.encodingFormat == "text/markdown"
             assert media.contentSize == "4.422 KB"
-    assert len(dataset.distribution) == 1
-    assert dataset.distribution[0].contentUrl == "http://www.hydroshare.org/resource/1ee81318135c40f587d9a3e5d689daf5"
-    assert dataset.distribution[0].encodingFormat == "application/zip"
 
 
 @pytest.mark.asyncio
-async def test_hydroshare_meta_adapter_collection(hydroshare_collection_metadata, dataset_model):
+async def test_hydroshare_collection_meta_adapter(hydroshare_collection_metadata, dataset_model):
     """Test the HydroshareMetaAdapter for Collection Resource"""
 
     adapter = RepositoryMetadataAdapter.get_adapter(repository=RepositoryType.HYDROSHARE)
@@ -105,6 +106,7 @@ async def test_hydroshare_meta_adapter_collection(hydroshare_collection_metadata
         dataset_model(**dataset.dict())
     except ValidationError as err:
         pytest.fail(f"Catalog dataset schema model validation failed: {str(err)}")
+
     assert isinstance(dataset, DatasetMetadataDOC)
     assert dataset.isPartOf == []
     assert len(dataset.hasPart) == 3
