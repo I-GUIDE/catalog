253f34de2c27cf7025b1da4e7b44824ffd7367a2
Scott Black <sblack@cuahsi.org>
2023-08-02 11:26:49 -0600

    add trailing slash to submission endpoint to prevent redirection

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -22,7 +22,7 @@ export const API_BASE = process.env.VUE_APP_API_URL || "";
 export const ENDPOINTS: { [key: string]: string } = {
   search: `${API_BASE}/discovery/search`,
   typeahead: `${API_BASE}/discovery/typeahead`,
-  submit: `${API_BASE}/catalog/dataset`,
+  submit: `${API_BASE}/catalog/dataset/`,
   dataset: `${API_BASE}/catalog/dataset`,
   schemaUrl: `${API_BASE}/schemas/schema.json`,
   uiSchemaUrl: `${API_BASE}/schemas/ui-schema.json`,
