a7f5db8820844e019355564ad5e21e5896c2406b
Maurier <mauriel.ramirez@gmail.com>
2024-05-02 10:00:28 -0600

    update build script and code cleanup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,27 +1,3 @@
-// import { ViteSSG } from "vite-ssg";
-// import App from "./App.vue";
-// import type { UserModule } from "./types";
-
-// import "./assets/css/global.scss";
-// import { routes } from "./routes";
-
-// // https://github.com/antfu/vite-ssg
-// export const createApp = ViteSSG(
-//   App,
-//   {
-//     routes,
-//     base: import.meta.env.BASE_URL,
-//   },
-//   (ctx) => {
-//     // install all modules under `modules/`
-//     Object.values(
-//       import.meta.glob<{ install: UserModule }>("./modules/*.ts", {
-//         eager: true,
-//       }),
-//     ).forEach((i) => i.install?.(ctx));
-//   },
-// );
-
 import "./assets/css/global.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,7 +1,3 @@
-import type { ViteSSGContext } from "vite-ssg";
-
-export type UserModule = (ctx: ViteSSGContext) => void;
-
 export interface IResult {
   id: string;
   creator: string[];
